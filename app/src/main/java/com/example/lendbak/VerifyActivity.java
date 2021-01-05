ed834bce0ef213946ccf4a97e0522564f0e694ff
david-osas <azamegbeosarumense@gmail.com>
2021-01-05 20:51:17 +0100

    Add dummy home activity

--- app/src/main/java/com/example/lendbak/VerifyActivity.java
+++ app/src/main/java/com/example/lendbak/VerifyActivity.java
@@ -2,6 +2,7 @@ package com.example.lendbak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,9 @@ public class VerifyActivity extends AppCompatActivity {
             binding.verifyText.setText(R.string.verifyPhoneText);
             binding.verifyOtp.setVisibility(View.VISIBLE);
             button.setText(R.string.verifyBtn);
+        }else{
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
         }
     }
 }
